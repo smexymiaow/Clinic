17d6fe56c9f523fa35797b28af5008fd1369b8fa
neonexxa <firdaus@cyberhax.club>
2017-06-10 16:04:49 +0800

    qq

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/homeqayyum', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home');
 
 Route::resource('appointment','AppointmentController');
 Route::resource('session','SessionController');
